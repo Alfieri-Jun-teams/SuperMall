3403d0fe3edcdfeb87a928b2fbb529a9a4bfe4e8
AlfieriChou <alfierichou@look.com>
2018-05-02 19:58:20 +0800

    migrations

--- migrations/user.js
+++ migrations/user.js
@@ -29,3 +29,45 @@ knex.schema.hasTable('goods').then(exists => {
     })
   }
 })
+
+knex.schema.hasTable('order').then(exists => {
+  if (!exists) {
+    return knex.schema.createTable('order', t => {
+      t.increments()
+      t.string('goods_id').comment('关联商品id')
+      t.string('user_id').comment('关联用户id')
+      t.float('amount', 9, 1).comment('数目')
+      t.boolean('is_fahuo').comment('是否发货')
+      t.string('express_name').comment('快递公司')
+      t.string('express_serial').comment('快递单号')
+      t.timestamp('created_at').defaultTo(knex.fn.now()).comment('创建时间')
+      t.datetime('updated_at').comment('更新时间')
+      t.datetime('deleted_at').comment('逻辑删除时间')
+      t.comment('订单信息表')
+    })
+  }
+})
+
+knex.schema.hasTable('admin').then(exists => {
+  if (!exists) {
+    return knex.schema.createTable('admin', t => {
+      t.increments()
+      t.string('phone', 11).comment('手机号')
+      t.string('password').comment('密码')
+      t.comment('管理员信息表')
+    })
+  }
+})
+
+knex.schema.hasTable('cart').then(exists => {
+  if (!exists) {
+    return knex.schema.createTable('cart', t => {
+      t.increments()
+      t.string('goods_id').comment('关联商品id')
+      t.float('amount', 9, 1).comment('数目')
+      t.timestamp('created_at').defaultTo(knex.fn.now()).comment('创建时间')
+      t.datetime('updated_at').comment('更新时间')
+      t.comment('购物车信息表')
+    })
+  }
+})
