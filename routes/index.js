349e6d6ac59c2d1f6e001dc6f989396213b65b1f
AlfieriChou <alfierichou@look.com>
2018-05-05 14:23:20 +0800

    update route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ import { adminLogin } from '../controller/admin'
 import { searchCart, createCart, getCart, putCart, destroyCart } from '../controller/cart'
 import { searchGoods, createGoods, getGoods, putGoods, destroyGoods } from '../controller/goods'
 import { searchOrder, createOrder, getOrder, putOrder, destroyOrder } from '../controller/order'
-import { verify } from '../common/token'
 
 const api = express.Router()
 
@@ -12,31 +11,31 @@ const api = express.Router()
 api.route('/users').get(searchUser)
 api.route('/users').post(createUser)
 api.route('/users/:id').get(getUser)
-api.route('/users/:id').put(verify, putUser)
-api.route('/users/:id').delete(verify, delUser)
+api.route('/users/:id').put(putUser)
+api.route('/users/:id').delete(delUser)
 
 // 管理员登录
 api.route('/admin/login').post(adminLogin)
 
 // 购物车
 api.route('/cart').get(searchCart)
-api.route('/cart').post(verify, createCart)
-api.route('/cart/:id').get(verify, getCart)
-api.route('/cart/:id').put(verify, putCart)
-api.route('/cart/:id').delete(verify, destroyCart)
+api.route('/cart').post(createCart)
+api.route('/cart/:id').get(getCart)
+api.route('/cart/:id').put(putCart)
+api.route('/cart/:id').delete(destroyCart)
 
 // 商品 (需要管理员权限)
 api.route('/goods').get(searchGoods)
-api.route('/goods').post(verify, createGoods)
+api.route('/goods').post(createGoods)
 api.route('/Goods/:id').get(getGoods)
-api.route('/Goods/:id').put(verify, putGoods)
-api.route('/Goods/:id').delete(verify, destroyGoods)
+api.route('/Goods/:id').put(putGoods)
+api.route('/Goods/:id').delete(destroyGoods)
 
 // 订单
-api.route('/order').get(verify, searchOrder)
-api.route('/order').post(verify, createOrder)
+api.route('/order').get(searchOrder)
+api.route('/order').post(createOrder)
 api.route('/order/:id').get(getOrder)
-api.route('/order/:id').put(verify, putOrder)
-api.route('/order/:id').delete(verify, destroyOrder)
+api.route('/order/:id').put(putOrder)
+api.route('/order/:id').delete(destroyOrder)
 
 export default api
