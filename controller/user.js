f947764641056ab12740b87bfec0c49344c71f1f
AlfieriChou <alfierichou@look.com>
2018-05-11 10:41:29 +0800

    update user module

--- controller/user.js
+++ controller/user.js
@@ -10,6 +10,7 @@ import Base64 from 'crypto-js/enc-base64'
 import SHA256 from 'crypto-js/sha256'
 import _ from 'lodash'
 
+// 数据分页功能还未添加
 const searchUser = async (req, res) => {
   const params = req.query
 
@@ -55,31 +56,35 @@ const createUser = async (req, res) => {
 }
 
 const getUser = async (req, res) => {
-  const params = {_id: req.params.id}
+  const params = {id: req.params.id}
   const user = await knex('users').where(params).whereNull('deleted_at').first()
 
   res.json(Response('用户查询成功', 1, user))
 }
 
+// (禁用用户和管理员权限还未添加) 权限问题可以封装到common里边方便以后的order和cart模块
 const putUser = async (req, res) => {
   const updateUser = Object.assign({updated_at: new Date()}, req.body)
   const params = Object.assign(req.body, req.params)
+  const account = req.session.account
+  if (account.user_type !== 'user') {
+    return res.status(400).send(Response('该账号不是用户', 0))
+  }
+  if (account.user_id !== params.id) {
+    return res.status(400).send(Response('权限不够', 1))
+  }
   const exist = await knex('users').where({_id: params.id}).whereNull('deleted_at').first()
-
   if (!exist) {
     return res.status(400).send(Response('用户不存在', 0))
   }
-
   const updateResult = await knex('users').where({_id: params.id}).update(updateUser)
-
   updateUser.updateResult = updateResult
-
   res.json(Response('用户信息修改成功', 1, updateUser))
 }
 
+// account表需要添加禁用用户字段
 const delUser = async (req, res) => {
   const exist = await knex('users').where({_id: req.params.id}).whereNull('deleted_at').first()
-
   if (!exist) {
     return res.status(400).send(Response('用户不存在', 0))
   }
@@ -87,7 +92,6 @@ const delUser = async (req, res) => {
   const deleteUser = await knex('users')
     .where({_id: req.params.id})
     .update({deleted_at: new Date()})
-
   res.json(Response('用户已删除', 1, deleteUser))
 }
 
