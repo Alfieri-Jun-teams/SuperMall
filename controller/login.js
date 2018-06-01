a656b28d9c5eb523986d09c92a9e3af272dd02f7
AlfieriChou <alfierichou@look.com>
2018-06-01 17:29:17 +0800

    add service

--- controller/login.js
+++ controller/login.js
@@ -1,36 +1,11 @@
-import { knex } from '../knex/mysql'
-import { Response } from '../common/Response'
-import { firstSecret, lastSecret } from '../config'
-import jwt from 'jsonwebtoken'
-import moment from 'moment'
-import Base64 from 'crypto-js/enc-base64'
-import SHA256 from 'crypto-js/sha256'
-import { loginLogger } from '../common/tracerlog'
 import { Login } from '../models/login'
 import Joi from 'joi'
-
-const createToken = (phone, password) => {
-  let payload = {
-    sub: {phone, password},
-    exp: moment().add(7, 'day').unix()
-  }
-  return jwt.sign(payload, firstSecret)
-}
+import { userLogin } from '../service/login'
 
 const login = async (req, res) => {
   const params = req.body
   Joi.validate(params, Login)
-  const saltPassword = firstSecret + params.password + lastSecret
-  const compare = Base64.stringify(SHA256(saltPassword))
-  const account = await knex('account').where('phone', params.phone).first()
-  if (account.password === compare) {
-    loginLogger.log(params.phone + '登录成功')
-    req.session.account = account
-    // logger.info({time: new Date(), userLogin: params.phone, message: '用户登录成功'})
-    return res.status(200).send(Response('用户验证成功', 1, createToken(account.phone, account.password)))
-  }
-  loginLogger.error(params.phone + '登录失败')
-  return res.status(400).send(Response('用户验证失败', '0'))
+  userLogin(params, req, res)
 }
 
 export {
---
+++ service/user.js
@@ -0,0 +1,27 @@
+const index = async (params, req, res) => {
+
+}
+
+const create = async (params, req, res) => {
+
+}
+
+const show = async (params, req, res) => {
+
+}
+
+const update = async (params, req, res) => {
+
+}
+
+const destroy = async (params, req, res) => {
+
+}
+
+export {
+  index,
+  create,
+  show,
+  update,
+  destroy
+}
