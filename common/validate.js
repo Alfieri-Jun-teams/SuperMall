5fcbffeac36cff0ca32dc942b2ad6d4294c2ba55
AlfieriChou <alfierichou@look.com>
2018-06-21 22:59:06 +0800

    add enjoi to validate

--- common/validate.js
+++ common/validate.js
@@ -1,10 +1,10 @@
 import Joi from 'joi'
+import Enjoi from 'enjoi'
 
-const validate = async (params, model, next) => {
-  const result = await Joi.validate(params, model)
-  if (!result.error) {
-    next()
-  } else {
+const validate = async (params, model) => {
+  const schema = Enjoi(model)
+  const result = await Joi.validate(params, schema)
+  if (result.error) {
     throw result.error.details[0].message
   }
 }
--- controller/login.js
+++ controller/login.js
@@ -5,12 +5,10 @@ import { userLogin } from '../service/login'
 const login = async (req, res) => {
   const params = req.body
   try {
-    console.log(Login)
     validate(params, Login)
-    console.log('111111')
     await userLogin(params, req, res)
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).send('登录失败')
   }
 }
 
