ad90e86250acec2c5773cd5bffd325bb4f94a834
AlfieriChou <alfierichou@look.com>
2018-06-24 11:15:20 +0800

    standard user service

--- controller/user.js
+++ controller/user.js
@@ -14,24 +14,38 @@ const create = async (req, res) => {
     validate(params, User)
     await userService.create(params, req, res)
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).send('用户创建失败')
   }
 }
 
 const show = async (req, res) => {
   const params = req.params
-  await userService.show(params, req, res)
+  try {
+    validate(params, User)
+    await userService.show(params, req, res)
+  } catch (err) {
+    res.status(400).send('用户创建失败')
+  }
 }
 
 const update = async (req, res) => {
-  const updateUser = Object.assign({updated_at: new Date()}, req.body)
   const params = Object.assign(req.body, req.params)
-  await userService.update(updateUser, params, req, res)
+  try {
+    validate(params, User)
+    await userService.update(params, req, res)
+  } catch (err) {
+    res.status(400).send('用户创建失败')
+  }
 }
 
 const destroy = async (req, res) => {
   const params = req.params
-  await userService.destroy(params, req, res)
+  try {
+    validate(params, User)
+    await userService.destroy(params, req, res)
+  } catch (err) {
+    res.status(400).send('用户创建失败')
+  }
 }
 
 export {
